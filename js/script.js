5ddde5599e1cff6f42aa0a12a70baec6909228c4
sslaguerta <sslaguerta@sscgi.com>
2025-05-22 17:23:18 +0800

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function runTypingEffect() {
+  const text = "I am Syrill Dex Laguerta.";
+  const typingElement = document.getElementById("typing-text");
+  const typingDelay = 100;
+
+  typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingElement.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+document.addEventListener("DOMContentLoaded", runTypingEffect);
